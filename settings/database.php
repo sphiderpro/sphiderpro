f11241bb549e5621ec407557f2610d89af155181
Vyacheslav Anzhiganov <vanzhiganov@ya.ru>
2017-01-11 20:21:25 +0300

    issue #4

--- settings/database.php
+++ settings/database.php
@@ -1,19 +1,13 @@
 <?php
-	$database="sphider";
-	$mysql_user = "root";
-	$mysql_password = ""; 
-	$mysql_host = "localhost";
-	$mysql_table_prefix = "";
+include dirname(__FILE__).'/database_conf.php';
 
+$success = mysql_pconnect ($mysql_host, $mysql_user, $mysql_password);
 
-
-	$success = mysql_pconnect ($mysql_host, $mysql_user, $mysql_password);
-	if (!$success)
+if (!$success)
 		die ("<b>Cannot connect to database, check if username, password and host are correct.</b>");
-    $success = mysql_select_db ($database);
-	if (!$success) {
+    $success = mysql_select_db($database);
+
+if (!$success) {
 		print "<b>Cannot choose database, check if database name is correct.";
 		die();
-	}
-?>
-
+}
