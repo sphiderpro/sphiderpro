ff515a979bad11f370e81f2d92eb8b7fa5747c7c
vanzhiganov <vanzhiganov@ya.ru>
2017-01-11 18:26:59 +0300

    resolving issue #1

--- admin/auth.php
+++ admin/auth.php
@@ -1,14 +1,18 @@
-<?php 
-	error_reporting(E_ERROR | E_PARSE);	
-	$admin = "admin";
-	$admin_pw = "admin";
+<?php
+error_reporting(E_ERROR | E_PARSE);
+$admin = "admin";
+$admin_pw = "admin";
 
-	session_start();
-	
-if (isset($_POST['user']) && isset($_POST['pass'])) {
 
-	$username = $_POST['user'];
-	$password = $_POST['pass'];
+function isCommandLineInterface() {
+    return (php_sapi_name() === 'cli');
+}
+
+if (!isCommandLineInterface()) {
+    session_start();
+    if (isset($_POST['user']) && isset($_POST['pass'])) {
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
 	if (($username == $admin) && ($password ==$admin_pw)) {
 		$_SESSION['admin'] = $username;
 		$_SESSION['admin_pw'] = $password;
@@ -18,21 +22,19 @@ if (isset($_POST['user']) && isset($_POST['pass'])) {
 } elseif ((isset($_SESSION['admin']) && isset($_SESSION['admin_pw']) &&$_SESSION['admin'] == $admin && $_SESSION['admin_pw'] == $admin_pw ) || (getenv("REMOTE_ADDR")=="")) {
 
 } else {
-	
-	?>
+?>
 	<html>
 	<head>
 	<title>Sphider Admin Login</title>
 		<LINK REL=STYLESHEET HREF="admin.css" TYPE="text/css">
 	</head>
-
 	<body>
 	<center>
 	<br><br>
-	
+
 	<fieldset style="width:30%;"><legend><b>Sphider Admin Login</b></legend>
 	<form action="auth.php" method="post">
-	
+
 	<table>
 	<tr><td>Username</td><td><input type="text" name="user"></td></tr>
 	<tr><td>Password</td><td><input type="password" name="pass"></td></tr>
@@ -43,12 +45,10 @@ if (isset($_POST['user']) && isset($_POST['pass'])) {
 	</center>
 	</body>
 	</html>
-	<?php 
+	<?php
 	exit();
+    }
 }
-
 
 $settings_dir = "../settings";
 include "$settings_dir/database.php";
-
-?>
